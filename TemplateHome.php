26c9c71266c961c1be69e4bb2c6676b3976b171d
1ndoryu <andoryyu@gmail.com>
2025-04-26 16:09:25 -0400

    *

--- TemplateHome.php
+++ TemplateHome.php
@@ -6,6 +6,7 @@ Template Name: Home
 use Glory\Class\ContentManager;
 use Glory\Components\LogoHelper;
 use Glory\Components\EmailFormBuilder;
+use Glory\Components\FormModalBuilder;
 
 
 get_header();
@@ -32,11 +33,44 @@ get_header();
                 </p>
             </div>
 
-            <?php EmailFormBuilder::displayWithModal([
-                'form_id' => 'newsletter-footer',
+            <?php EmailFormBuilder::display([
+                'form_id' => 'newsletter-footer', // Este ID debe coincidir con 'target_form_id' abajo
                 'email_placeholder' => 'Your email address',
                 'submit_value' => 'Join Us',
+                'nonce_action' => 'glory_email_signup_action', // Nonce para la acción de registro de email
+                'modal_target_id' => 'newsletter-footer-modal' // Asegúrate que el ID del modal a abrir es correcto
             ]); ?>
+
+            <?php FormModalBuilder::display(
+                [
+                    'modal_id' => 'newsletter-footer-modal', // ID de este modal
+                    'modal_title' => 'Complete Your Profile',
+                    'ajax_action' => 'glory_update_user_details', // La acción AJAX que este form dispara
+                    'nonce_action' => 'glory_update_user_details_nonce', // <<<==== NONCE ACTION ESPECÍFICO para la acción de arriba
+                    'target_form_id' => 'newsletter-footer', // ID del form original (EmailFormBuilder) para mostrar mensaje de éxito
+                    'hidden_fields' => [
+                        ['name' => 'user_id', 'data_attr' => 'glory-user-id-input'] // JS buscará 'data-glory-user-id-input'
+                    ],
+                    'fields' => [
+                        [
+                            'name' => 'first_name',
+                            'label' => 'First Name',
+                            'required' => true,
+                            'id' => 'newsletter-footer-modal-first-name' // IDs explícitos son buenos
+                        ],
+                        [
+                            'name' => 'last_name',
+                            'label' => 'Last Name',
+                            'required' => false,
+                            'id' => 'newsletter-footer-modal-last-name'
+                        ],
+                    ],
+                    'submit_value' => 'Save Profile',
+                    'submit_data_wait' => 'Saving...',
+                    'failure_message' => 'Could not save details. Please try again.' // Mensaje para errores *dentro* del modal
+                ]
+            ); ?>
+
 
         </div>
         <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/App/css/logoHelper.css" />
